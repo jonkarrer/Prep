10fc2df7e960158b5bc27b1e85b173ddbe45486e
jonkarrer <karrerjon@gmail.com>
2023-10-23 01:29:38 +0000

    init

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use poem::{get, handler, listener::TcpListener, web::Path, IntoResponse, Route, Server};
+
+#[handler]
+fn hello(Path(name): Path<String>) -> String {
+    println("request accepted");
+    format!("hello: {}", name)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), std::io::Error> {
+    println!("starting server");
+    let app = Route::new().at("/hello/:name", get(hello));
+    Server::new(TcpListener::bind("127.0.0.1:8000"))
+        .run(app)
+        .await
+}

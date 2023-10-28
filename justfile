f943bb65f2016367b34838e131dd3c0ce81c7bbe
jkarrer <jkarrer@protect247.com>
2023-10-28 09:35:38 -0400

    chore: add migration command to justfile

--- justfile
+++ justfile
@@ -1,11 +1,21 @@
+# ---- Tests ----
+test-gateway:
+    just start-db;
+    sleep 10;
+    -cargo test gateway
+    just stop-db;
+
+# ---- DEV DATABASE ----
+echo-db-url:
+    @source .env.dev && echo "${DATABASE_URL}"
+
 start-db:
-   bash ./scripts/init_db.sh 
+   bash ./scripts/init_db.sh
 
 stop-db:
     docker kill mysql;
 
-test-gateway:
-    just start-db;
-    sleep 10;
-    cargo test gateway
-    just stop-db;
+# migrations
+migrate-add file_name:
+    @source .env.dev \
+    && sqlx migrate add {{file_name}}
--- scripts/init_db.sh
+++ scripts/init_db.sh
@@ -3,11 +3,28 @@
 set -x
 # stop on any error from any command in a pipeline. 
 set -eo pipefail
+
+# check for dependancies
+# if ! [ -x "$(command -v mysql)" ]; then
+#   echo >&2 "Error: mysql is not installed."
+#   exit 1
+# fi
+
+# if ! [ -x "$(command -v sqlx)" ]; then
+#   echo >&2 "Error: sqlx is not installed."
+#   echo >&2 "Use:"
+#   echo >&2 "    cargo install --version=0.6.0 sqlx-cli --no-default-features --features postgres"
+#   echo >&2 "to install it."
+#   exit 1
+# fi
+
+# set env vars
 DB_USER=${MYSQL_USER:="root"}
 DB_PASSWORD=${MYSQL_PASSWORD:="my-secret-pw"}
 DB_NAME=${MYSQL_DB:="mysql"}
 DB_PORT=${MYSQL_PORT:="3306"}
 
+# run docker command
 docker run \
     --detach \
     --name mysql \
@@ -15,6 +32,12 @@ docker run \
     -p "${DB_PORT}":3306 \
     mysql:latest;
 
-sleep 6
+# Keep pinging MySQL until it's ready to accept commands
+until mysql -h 127.0.0.1 -u "${DB_USER}" -p"${DB_PASSWORD}" -P "${DB_PORT}" -D "${DB_NAME}" -e 'SELECT 1'; do
+  >&2 echo "MySQL is still unavailable - sleeping"
+  sleep 1
+done
 
-export DATABASE_URL=mysql://${DB_USER}:${DB_NAME}@localhost:${DB_PORT}/${DB_PASSWORD}
+# create migration with sqlx
+# export DATABASE_URL=mysql://${DB_USER}:${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+# sqlx database create

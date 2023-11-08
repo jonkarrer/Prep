a80e07a6e6aa14be6323d32abb6ddec503d9ebc0
jkarrer <jkarrer@protect247.com>
2023-11-08 15:46:25 -0500

    fix: static file path

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #![forbid(unsafe_code)]
-use poem::{listener::TcpListener, middleware::AddData, EndpointExt, Result, Server};
+use poem::{
+    endpoint::StaticFilesEndpoint, listener::TcpListener, middleware::AddData, EndpointExt, Result,
+    Server,
+};
 use prep::{
     application::helper::get_configuration,
     domain::config::Settings,
@@ -18,7 +21,15 @@ async fn main() -> Result<(), std::io::Error> {
     let address = format!("{}:{}", application_host, application_port);
     let listener = TcpListener::bind(address);
     let db = db().await;
-    let router = router().with(AddData::new(db)).with(Log);
+    let router = router()
+        .nest(
+            "/",
+            StaticFilesEndpoint::new("./")
+                .show_files_listing()
+                .index_file("index.html"),
+        )
+        .with(AddData::new(db))
+        .with(Log);
 
     Server::new(listener).run(router).await
 }

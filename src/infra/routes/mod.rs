618584ec1f9f858fb89f13d550f001c323addf42
jkarrer <jkarrer@protect247.com>
2023-11-08 15:15:59 -0500

    feat: serve static html, css, and js files

--- src/infra/routes/mod.rs
+++ src/infra/routes/mod.rs
@@ -1,5 +1,6 @@
 mod get;
 mod post;
+use poem::endpoint::StaticFilesEndpoint;
 use poem::{get, post, EndpointExt, Route};
 
 use self::get::{handle_select_recipe_by_id, health_check};
@@ -21,6 +22,12 @@ pub fn router() -> Route {
     let app = Route::new()
         .nest("/recipe", recipe_routes)
         .nest("/usr", user_routes)
+        .nest(
+            "/",
+            StaticFilesEndpoint::new("./src/web")
+                .show_files_listing()
+                .index_file("index.html"),
+        )
         .at("/health_check", get(health_check));
 
     app

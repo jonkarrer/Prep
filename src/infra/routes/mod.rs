7024124d34cdd2d04b70898f2b5babaa06e06b9d
jkarrer <jkarrer@protect247.com>
2023-11-19 20:52:32 -0500

    add: landing page route

--- src/infra/routes/mod.rs
+++ src/infra/routes/mod.rs
@@ -1,20 +1,17 @@
 mod auth;
 mod dash;
-mod health_check;
 mod recipe;
 mod user;
-
-use self::health_check::handle_health_check;
-use poem::{endpoint::StaticFilesEndpoint, get, Route};
+use poem::{endpoint::StaticFilesEndpoint, Route};
 
 pub fn router() -> Route {
-    let app = Route::new()
+    Route::new()
         .nest("/recipe", recipe::use_recipe_routes())
         .nest("/auth", auth::use_auth_routes())
         .nest("/usr", user::use_user_routes())
         .nest("/dash", dash::use_dash_routes())
-        .nest("/", StaticFilesEndpoint::new("./src/web"))
-        .at("/health_check", get(handle_health_check));
-
-    app
+        .nest(
+            "/",
+            StaticFilesEndpoint::new("./src/web").index_file("index.html"),
+        )
 }

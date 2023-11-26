573e0d1d4322d3e07ec4d969d3bae13c9b316c22
jkarrer <jkarrer@protect247.com>
2023-11-26 18:51:08 -0500

    chore: add notes for email client

---
+++ src/infra/service/email_client.rs
@@ -0,0 +1,64 @@
+// pub struct EmailClient {
+//     http_client: Client,
+//     base_url: String,
+//     sender: SubscriberEmail,
+//     authorization_token: Secret<String>,
+// }
+
+// impl EmailClient {
+//     pub fn new(
+//         base_url: String,
+//         sender: SubscriberEmail,
+//         authorization_token: Secret<String>,
+//         timeout: std::time::Duration,
+//     ) -> Self {
+//         let http_client = Client::builder().timeout(timeout).build().unwrap();
+//         Self {
+//             http_client,
+//             base_url,
+//             sender,
+//             authorization_token,
+//         }
+//     }
+
+//     pub async fn send_email(
+//         &self,
+//         recipient: &SubscriberEmail,
+//         subject: &str,
+//         html_content: &str,
+//         text_content: &str,
+//     ) -> Result<(), reqwest::Error> {
+//         let url = format!("{}/email", self.base_url);
+//         let request_body = SendEmailRequest {
+//             from: self.sender.as_ref(),
+//             to: recipient.as_ref(),
+//             subject,
+//             html_body: html_content,
+//             text_body: text_content,
+//         };
+//         self.http_client
+//             .post(&url)
+//             .header(
+//                 "X-Postmark-Server-Token",
+//                 self.authorization_token.expose_secret(),
+//             )
+//             .json(&request_body)
+//             .send()
+//             .await?
+//             .error_for_status()?;
+//         Ok(())
+//     }
+// }
+
+// #[derive(serde::Serialize)]
+// #[serde(rename_all = "PascalCase")]
+// struct SendEmailRequest<'a> {
+//     from: &'a str,
+//     to: &'a str,
+//     subject: &'a str,
+//     html_body: &'a str,
+//     text_body: &'a str,
+// }
+
+// Link to setup tutorial
+// https://www.lpalmieri.com/posts/how-to-write-a-rest-client-in-rust-with-reqwest-and-wiremock/
--- src/infra/service/mod.rs
+++ src/infra/service/mod.rs
@@ -1,2 +1,5 @@
 mod decode_bearer_token;
 pub use decode_bearer_token::*;
+
+mod email_client;
+pub use email_client::*;

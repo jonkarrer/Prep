ac8962e66f684d2dcf20c8270ad215a4e17f6bac
jkarrer <jkarrer@protect247.com>
2023-11-27 20:23:31 -0500

    refactor: put both forms in auth page

--- src/infra/routes/auth/mod.rs
+++ src/infra/routes/auth/mod.rs
@@ -5,7 +5,7 @@ mod register;
 use crate::infra::middleware::AuthGuard;
 use login::*;
 use logout::*;
-use poem::{endpoint::StaticFileEndpoint, get, EndpointExt, Route};
+use poem::{endpoint::StaticFileEndpoint, get, post, EndpointExt, Route};
 use register::*;
 
 pub fn use_auth_routes() -> Route {
@@ -15,7 +15,7 @@ pub fn use_auth_routes() -> Route {
             get(StaticFileEndpoint::new("src/web/pages/auth/auth.html")).post(handle_register),
         )
         .at("/register", get(handle_register_ui).post(handle_register))
-        .at("/login", get(handle_login_ui).post(handle_login))
+        .at("/login", post(handle_login))
         .at(
             "/logout",
             get(handle_logout_ui).post(handle_logout).with(AuthGuard),

04ec4fcf72239e35db07889e962611e5d6e3b50d
jkarrer <jkarrer@protect247.com>
2023-12-11 16:00:26 -0500

    wip: create an app feel

--- src/web/scripts/recipe/create/section_swap.js
+++ src/web/scripts/recipe/create/section_swap.js
@@ -6,61 +6,62 @@ let ingredientController = document.getElementById("ingredient_controller");
 let directionSection = document.getElementById("direction_section");
 let directionController = document.getElementById("direction_controller");
 
+function showElements(elements) {
+  for (let el of elements) {
+    el.classList.remove("hidden");
+    el.classList.add("block");
+  }
+}
+function hideElements(elements) {
+  for (let el of elements) {
+    el.classList.remove("block");
+    el.classList.add("hidden");
+  }
+}
+
+function hideIngredientSection() {
+  hideElements([ingredientSection, ingredientController]);
+}
+
+function hideGeneralSection() {
+  hideElements([generalSection]);
+}
+
+function hideDirectionSection() {
+  hideElements([directionSection, directionController]);
+}
+
 function showGeneralSection() {
-  generalSection.classList.remove("hidden");
-  generalSection.classList.add("block");
-
-  directionSection.classList.remove("block");
-  directionController.classList.remove("block");
-  directionSection.classList.add("hidden");
-  directionController.classList.add("hidden");
-
-  ingredientSection.classList.remove("block");
-  ingredientSection.classList.add("hidden");
-  ingredientController.classList.remove("block");
-  ingredientController.classList.add("hidden");
-
-  updateRecipePreview();
+  showElements([generalSection]);
 }
 
 function showIngredientSection() {
-  ingredientSection.classList.remove("hidden");
-  ingredientController.classList.remove("hidden");
-  ingredientSection.classList.add("block");
-  ingredientController.classList.add("block");
-
-  generalSection.classList.remove("block");
-  generalSection.classList.add("hidden");
+  showElements([ingredientSection, ingredientController]);
 }
 
 function showDirectionSection() {
-  directionSection.classList.remove("hidden");
-  directionController.classList.remove("hidden");
-  directionSection.classList.add("block");
-  directionController.classList.add("block");
-
-  generalSection.classList.remove("block");
-  generalSection.classList.add("hidden");
-
-  ingredientSection.classList.remove("block");
-  ingredientSection.classList.add("hidden");
-  ingredientController.classList.remove("block");
-  ingredientController.classList.add("hidden");
+  showElements([directionSection, directionController]);
 }
 
 function showSection(section_id) {
-  console.log("swap", section_id);
   switch (section_id) {
     case "general_section":
       showGeneralSection();
+      hideIngredientSection();
+      hideDirectionSection();
+      updateRecipePreview();
       break;
 
     case "ingredient_section":
       showIngredientSection();
+      hideGeneralSection();
+      hideDirectionSection();
       break;
 
     case "direction_section":
       showDirectionSection();
+      hideGeneralSection();
+      hideIngredientSection();
       break;
 
     default:
